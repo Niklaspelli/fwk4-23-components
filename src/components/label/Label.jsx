8cb90b94733581c735302fafe85869035305e41e
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-02 10:30:30 +0200

    Label component added

---
+++ src/components/label/Label.jsx
@@ -0,0 +1,5 @@
+const Label = () => {
+  return <label>Username:</label>;
+};
+
+export default Label;
---
+++ src/components/label/Label.stories.jsx
@@ -0,0 +1,10 @@
+import Label from "./Label";
+
+export default {
+  title: "FKW, Label",
+  component: Label,
+};
+
+export const Default = () => {
+  return <Label />;
+};
