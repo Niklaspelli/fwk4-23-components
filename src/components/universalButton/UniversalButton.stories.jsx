b4a435325e45f8dcb0bcb9060a6396990356af61
hallstrom91 <simonhallstrom@live.se>
2024-09-04 14:26:21 +0200

    #70: add Storybook stories for UniversalButton component

---
+++ src/components/universalButton/UniversalButton.stories.jsx
@@ -0,0 +1,40 @@
+import UniversalButton from "@universalButton/UniversalButton";
+
+export default {
+  title: "FWK/UniversalButton",
+  component: UniversalButton,
+};
+
+export const Default = {
+  args: {
+    title: "Default",
+    type: "submit",
+  },
+};
+
+export const MissingTitle = {
+  args: {
+    type: "submit",
+  },
+};
+
+export const MissingType = {
+  args: {
+    title: "Missing Type",
+  },
+};
+
+export const MissingOnClick = {
+  args: {
+    title: "Missing onClick",
+    type: "button",
+  },
+};
+
+export const validButton = {
+  args: {
+    title: "Valid Button",
+    type: "button",
+    onClick: () => console.log("Valid Button Clicked"),
+  },
+};
