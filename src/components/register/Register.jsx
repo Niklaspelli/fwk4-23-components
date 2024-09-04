945a50b32692b1eb01059d5719d943716a107172
88Robert <robert.magnusson1988@gmail.com>
2024-09-04 11:31:46 +0200

    connect from to reg task 15

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,11 +1,13 @@
-import Logo from '../logo/Logo';
+import Logo from "../logo/Logo";
+import Form from "../login/Form";
 
 const Register = () => {
-	return (
-		<div>
-			<Logo /> <h1>Register</h1>
-		</div>
-	);
+  return (
+    <div>
+      <Logo /> <h1>Register</h1>
+      <Form fields={["username", "email", "password", "repeatPassword"]} />
+    </div>
+  );
 };
 
 export default Register;
