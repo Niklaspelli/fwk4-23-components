fe464ffeee7cccb34307829daba554b6967a6dab
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-09-09 11:06:20 +0200

    Create a logo image #94

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,5 +1,11 @@
+import logoImage from '../../assets/LOGO.webp'; 
 const Logo = () => {
-  return <h1>I-Organize</h1>;
+  return (
+    <div>
+      <img src={logoImage} alt="I-Organize Logo" />
+      <h1>I-Organize</h1>
+    </div>
+  );
 };
 
-export default Logo;
+export default Logo
