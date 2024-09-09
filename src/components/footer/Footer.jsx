829399b47f9e79e24629ce332cc53491fb3127c9
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-09 10:16:45 +0200

    #98: Footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>----- Det här är en footer länk länk -----</div>;
+};
+
+export default Footer;
---
+++ src/components/footer/Footer.stories.jsx
@@ -0,0 +1,10 @@
+import Footer from "./Footer";
+
+export default {
+  title: "FWK/Footer",
+  component: Footer,
+};
+
+export const Default = () => {
+  return <Footer />;
+};
