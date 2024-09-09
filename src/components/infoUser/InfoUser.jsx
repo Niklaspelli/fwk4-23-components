d24dc408e463f632d5c2db9c6c579e7cfadb7378
hallstrom91 <simonhallstrom@live.se>
2024-09-09 11:46:41 +0200

    #105 - resolved import path error due to filename

---
+++ src/components/infoUser/InfoUser.jsx
@@ -0,0 +1,13 @@
+const InfoUser = () => {
+	return (
+		<>
+			{/* Lägg in form här istället */}
+			<div>Username:</div>
+			<div>Email:</div>
+			<div>Password:</div>
+			<div>Start date:</div>
+		</>
+	);
+};
+
+export default InfoUser;
---
+++ src/components/infoUser/InfoUser.stories.jsx
@@ -0,0 +1,8 @@
+import InfoUser from './InfoUser';
+
+export default {
+	title: 'FWK/InfoUser',
+	component: InfoUser
+};
+
+export const Default = {};
