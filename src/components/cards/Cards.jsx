8d17caa91a9a6439797ce2a39f42331f26ea12d5
Monchekin <barthelsonlina@gmail.com>
2024-09-11 11:18:53 +0200

    #101 - Added a text box to Cards

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React from 'react';
 import './Cards.modules.css';
 
 const Cards = ({ title, items }) => {
-  return (
-    <div className="card">
-      <h2>{title}</h2>
-      <ul>
-        {items && items.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className='card'>
+			<h2>{title}</h2>
+			<ul>
+				{items && items.map((item, index) => <li key={index}>{item}</li>)}
+			</ul>
+			<input
+				type='text'
+				placeholder='Skriv något här...'
+				style={{ width: '300px', height: '60px' }}
+			/>
+		</div>
+	);
 };
 
 export default Cards;
