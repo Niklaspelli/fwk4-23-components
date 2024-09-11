148f029cc99a4f3f1b909af0ff890f9eacf493d5
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-09-11 10:41:18 +0200

    #75 Different headlines created

--- src/components/headlines/HeadLines.jsx
+++ src/components/headlines/HeadLines.jsx
@@ -1,14 +1,25 @@
-const HeadLines = ({login, register, toDo, newTask, deleteTask, }) => {
-
-  // test commit
+const HeadLines = ({title, subTitle}) => {
   return (
     <>
-      <h1>{login}</h1>
-      <h1>{register}</h1>
-      <h2>{toDo}</h2>
-      <h3>{newTask}</h3>
-      <h3>{deleteTask}</h3>
-    </>
-  );
-};
+    <h1>{title}</h1>
+    <h3>{subTitle}</h3>
+    </> 
+  )
+}
+
 export default HeadLines;
+
+// const HeadLines = ({login, register, toDo, newTask, deleteTask, }) => {
+
+//   // test commit
+//   return (
+//     <>
+//       <h1>{login}</h1>
+//       <h1>{register}</h1>
+//       <h2>{toDo}</h2>
+//       <h3>{newTask}</h3>
+//       <h3>{deleteTask}</h3>
+//     </>
+//   );
+// };
+// export default HeadLines;
--- src/components/headlines/HeadLines.stories.jsx
+++ src/components/headlines/HeadLines.stories.jsx
@@ -5,16 +5,35 @@ export default {
     component: HeadLines,
 };
 
+export const Login = {
+    args: {
+        title: "Login HeadLine"
+    }
+}
+
+export const Register = {
+    args: {
+        title: "Register HeadLine"
+    }
+}
+
+export const Default = {
+    args: {
+        title: "Headline",
+        subTitle: "Sub headline"
+    }
+}
+
 // export const Default = () => {
 //     return <HeadLines />;
 // }
 
-export const Default = {
-args: {
-    login: "Login Page",
-    register: "Register Page",
-    toDo: "My Todos",
-    newTask: "Add New Task",
-    deleteTask: "Delete Task",
-}
-};
+// export const Default = {
+// args: {
+//     login: "Login Page",
+//     register: "Register Page",
+//     toDo: "My Todos",
+//     newTask: "Add New Task",
+//     deleteTask: "Delete Task",
+// }
+// };
