7026e6f4b731857a420ac6f9ba220883254fefa5
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-16 10:34:01 +0200

    #125 LoginContext

---
+++ src/components/login/LoginProvider.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from "react";
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({ children }) => {
+  const [useId, setUserId] = useState(null);
+
+  const loginFunction = async (username, password) => {
+    try {
+      const response = await fetch("https://your-api.com/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+      });
+      const result = await response.json();
+
+      if (response.ok) {
+        setUserId(result.userId);
+        console.log("Login successful, User ID:", result.userId);
+      } else {
+        console.error("Login failed:", result.message);
+      }
+    } catch (error) {
+      console.error("An error occurred during Login:", error);
+    }
+  };
+
+  return (
+    <LoginContext.Provider value={{ userId, loginFunction }}>
+      {children}
+    </LoginContext.Provider>
+  );
+};
+
+export const useLogin = () => {
+  return useContext(LoginContext);
+};
