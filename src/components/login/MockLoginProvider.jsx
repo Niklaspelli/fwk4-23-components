d8eaeb4f997b74a16ac4158abce7722663b452de
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-16 11:23:24 +0200

    #127 mockProvider created

--- src/components/login/MockLoginProvider.jsx
+++ src/components/login/MockLoginProvider.jsx
@@ -1,1 +1,29 @@
-import React, 
+import React, { createContext, useContext, useState } from "react";
+
+const MockLoginContext = createContext();
+
+export const MockLoginProvider = ({ children }) => {
+  const [userId, setUserId] = useState(null);
+
+  const mockLoginFunction = ({ children }) => {
+    if (username === "Iorganize" && password === "Iorganizetest") {
+      const mockUserId = "organize-uuid-222333";
+      setUserId(mockUserId);
+      console.log("Mock login successful. User ID:", mockUserId);
+    } else {
+      console.log("Mock login failed: Invalid username or password");
+    }
+  };
+
+  return (
+    <MockLoginContext.Provider
+      value={{ userId, loginFunction: mockLoginFunction }}
+    >
+      {children}
+    </MockLoginContext.Provider>
+  );
+};
+
+export const useMockLogin = () => {
+  return useContext(MockLoginContext);
+};
