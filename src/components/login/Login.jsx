00b1e449b0f970d6b611cea7dd81ef4b388661db
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-18 13:40:12 +0200

    #139 logic to Login.jsx

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,17 +1,38 @@
-import Form from './Form';
-import Switch from './Switch';
-import UniversalButton from '../subComponents/universalButton/UniversalButton';
-import HeadLines from '../subComponents/headlines/HeadLines';
+import Form from "./Form";
+import Switch from "./Switch";
+import UniversalButton from "../subComponents/universalButton/UniversalButton";
+import HeadLines from "../subComponents/headlines/HeadLines";
+import UserAtom from "./UserAtom.jsx";
+import PasswordAtom from "./PasswordAtom.jsx";
+import LoginButton from "./LoginButtonAtom.jsx";
 
-const Login = () => {
-	return (
-		<>
-			<HeadLines title='Login Page' />
-			<Form fields={['username', 'password']} />
+const Login = ({ loginFunction }) => {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLoginClick = () => {
+    if (loginFunction) {
+      loginFunction(user, password);
+    } else {
+      console.error("No login function provided");
+      console.error("User:", user);
+      console.error("Password:", password);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <HeadLines title="Login Page" />
+        <UserAtom onUserChange={setUser} />
+        <PasswordAtom onPasswordChange={setPassword} />
+        <LoginButton onClick={handleLoginClick} />
+        {/* <Form fields={['username', 'password']} />
 			<UniversalButton title='Login' type='submit' />
-			<Switch />
-		</>
-	);
+			<Switch /> */}
+      </div>
+    </>
+  );
 };
 
 export default Login;
