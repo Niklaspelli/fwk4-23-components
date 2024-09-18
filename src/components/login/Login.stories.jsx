988305fb649628d5c03ee5107ed6663db7904096
Niklas Pelli <niklaspelli22@gmail.com>
2024-09-18 13:48:01 +0200

    #140 logic added to login.stories

--- src/components/login/Login.stories.jsx
+++ src/components/login/Login.stories.jsx
@@ -1,8 +1,20 @@
-import Login from './Login'
+import Login from "./Login";
+import { LoginProvider } from "./LoginProvider.jsx";
+import { MockLoginProvider } from "./MockLoginProvider.jsx";
 
 export default {
-    title: 'Components/Login',
-    component: Login,
-}
+  title: "Components/Login",
+  component: Login,
+};
 
-export const Default = () => <Login />;
+export const Default = () => (
+  <MockLoginProvider>
+    <Login />
+  </MockLoginProvider>
+);
+
+export const WithRealProvider = () => (
+  <LoginProvider>
+    <Login />
+  </LoginProvider>
+);
