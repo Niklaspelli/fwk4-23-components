3cb6e65602d4a5c192e8509fc3344f27c7c65295
ellinor Jansson lande <ellinor_jansson@hotmail.com>
2024-09-18 11:14:51 +0200

    #96 changes styling in logo-text

--- src/components/subComponents/logo/Logo.jsx
+++ src/components/subComponents/logo/Logo.jsx
@@ -1,6 +1,8 @@
+import './Logo.css';
+
 const Logo = ({ logoSrc, altText }) => {
 	return (
-		<div>
+		<div className="Logo">
 			<img src={logoSrc} alt={altText} />
 			<h1>I-Organize</h1>
 		</div>
