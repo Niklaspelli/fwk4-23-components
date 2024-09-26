a0c6368529ede42177ef43cd717e977e99fccde9
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-09-26 13:05:11 +0200

    react & react-dom verions moved from dependencies to devDependencies in package.json file. Change react imports in components

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 import Form from "./Form";
 import Switch from "./Switch";
 import UniversalButton from "../subComponents/universalButton/UniversalButton";
