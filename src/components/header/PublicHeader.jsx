beddf14a67071f21c818d437f3c1673218260d75
Lina.barthelson@gmail.com <lina.barthelson@gmail.com>
2024-10-01 15:49:49 +0200

    #160- Private and public headers

---
+++ src/components/header/PublicHeader.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Logo from "../subComponents/logo/Logo";
+import styles from "./Header.module.css";
+import logosmall from "../../assets/logosmall.jpg";
+
+const PrivateHeader = () => {
+  // add auto selection of img-size based on screen-size to props value logoSrc
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.logoContainer}>
+          <Logo logoSrc={logosmall} altText="I-Organize Logo" />
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default PrivateHeader;
