ab0af44724bfe269918076cbd2581a95dca2504c
rosenholmjohanna <rosenholm.johanna88@gmail.com>
2024-10-03 10:12:06 +0200

    index.js file added with exports for taskboard, deletebutton, taskcard & column components

---
+++ src/components/subComponents/taskboard/index.js
@@ -0,0 +1,6 @@
+import Column from './Column';
+import DeleteButton from './DeleteButton';
+import TaskBoard from './TaskBoard';
+import TaskCard from './TaskCard';
+
+export {Column, DeleteButton, TaskBoard, TaskCard  }
